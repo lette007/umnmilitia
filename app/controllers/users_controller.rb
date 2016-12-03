7a1511eb7870e587b8b0b90584669c3cad5669df
lette007 <lette007@umn.edu>
2016-12-03 23:51:41 +0000

    activation message: add time estimate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Please check your email to activate your account - 
+      email will arrive in <50 sec."
       redirect_to about_path
     else
       render 'new'
