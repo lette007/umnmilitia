8866a528791053b3821ce0fb7df5143f468cc59f
lette007 <lette007@umn.edu>
2017-01-17 22:52:04 +0000

    alter root path and header

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
 
   get 'password_resets/edit'
 
-  root   'static_pages#about'
+  root   'users#new'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
-  get    '/nicole1', to: 'static_pages#nicole1'
-  get    '/nicole2', to: 'static_pages#nicole2'
-  get    '/nicole3', to: 'static_pages#nicole3'
-  get    '/ok',      to: 'static_pages#ok'
-  
   get    '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
