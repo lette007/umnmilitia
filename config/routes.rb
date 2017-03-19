e78e33fbcfcd4cf5838ba12cfe2aa5e7696edaa3
lette007 <lette007@umn.edu>
2017-03-19 11:10:47 +0000

    add get to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
+  get    '/nicole1', to: 'static_pages#nicole1'
   get    '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get   '/edit', to: 'users#edit'
